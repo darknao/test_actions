363adbebdeb87f2e91a355ffc7a1986fe81beb5c
darknao <naolwen@gmail.com>
2020-12-30 22:28:32 +0100

    generator

---
+++ gen.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+"""
+matrix:
+ include:
+ - name: 1.2-apache
+   os: ubuntu-latest
+   tags:
+   - 1.2
+   - 1.2-apache
+   context: apache/1.2
+ - name: 1.3-apache
+   os: ubuntu-latest
+   tags:
+   - 1.3
+   - 1.3-apache
+   - latest
+   context: apache/1.3
+"""
+
+import glob
+import json
+from distutils.version import StrictVersion
+from pprint import pprint
+
+dockerfiles = glob.glob("*/*/Dockerfile")
+l = []
+for df in dockerfiles:
+  variant,version,_ = df.split('/')
+  l.append({
+    'version': version,
+    'variant': variant,
+    })
+
+l.sort(key=lambda f: StrictVersion(f['version']))
+latest = l[-1]['version']
+print("latest is %s" % (latest,))
+pprint(l)
+strategy = {
+  "fail-fast": False,
+  "matrix": {
+    "include": [],
+    }}
+for im in l:
+  tags = []
+  if im['variant'] == "apache":
+    tags.append(im['version'])
+    if im['version'] == latest:
+      tags.append("latest")
+      tags.append("apache")
+  else:
+    tags.append("%s-%s" % (im['version'], im['variant']))
+    if im['version'] == latest:
+      tags.append(im['variant'])
+
+  strategy['matrix']['include'].append(
+    {
+      "name": "%s-%s" % (im['version'], im['variant']),
+      "os": "ubuntu-latest",
+      "tags:": tags,
+      "context": "%s/%s" % (im['variant'], im['version']),
+      })
+
+pprint(strategy)
+print("::set-output name=strategy::%s" % (json.dumps(strategy),))
