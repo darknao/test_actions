720c5328074502452fbf771eeb6c1214e4a2c0bc
darknao <naolwen@gmail.com>
2020-12-30 23:56:32 +0100

    fix tags

--- gen.py
+++ gen.py
@@ -59,7 +59,7 @@ for im in l[:2]:
     {
       "name": "%s-%s" % (im['version'], im['variant']),
       "os": "ubuntu-latest",
-      "tags:": tags,
+      "tags:": ",".join(tags),
       "context": "%s/%s" % (im['variant'], im['version']),
       "dockerfile": im['file'],
       })
