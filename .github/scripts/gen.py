69b34bdcde79375c9259a352dc7c3293f36c3f41
darknao <naolwen@gmail.com>
2020-12-31 13:21:33 +0100

    regen readme

--- .github/scripts/gen.py
+++ .github/scripts/gen.py
@@ -2,7 +2,12 @@
 import glob
 import json
 from distutils.version import StrictVersion
+from argparse import ArgumentParser
 from pprint import pprint
+
+parser = ArgumentParser()
+parser.add_argument("--set-output", default=False, dest='set_output', action='store_true', help="Set Github actions output")
+opts = parser.parse_args()
 
 dockerfiles = glob.glob("*/*/Dockerfile")
 l = []
@@ -16,13 +21,16 @@ for df in dockerfiles:
 
 l.sort(key=lambda f: StrictVersion(f['version']))
 latest = l[-1]['version']
-print("latest is %s" % (latest,))
-strategy = {
-  "max-parallel": 1,
-  "fail-fast": False,
-  "matrix": {
-    "include": [],
-    }}
+#print("latest is %s" % (latest,))
+if opts.set_output:
+  strategy = {
+    "max-parallel": 1,
+    "fail-fast": False,
+    "matrix": {
+      "include": [],
+      }}
+else:
+  strategy = []
 for im in l:
   tags = []
   if im['variant'] == "apache":
@@ -35,15 +43,26 @@ for im in l:
     if im['version'] == latest:
       tags.append(im['variant'])
 
-  strategy['matrix']['include'].append(
-    {
-      "name": "%s-%s" % (im['version'], im['variant']),
-      "os": "ubuntu-latest",
-      "cache": "%s-cache" % (im['variant'],),
-      "tags": ",".join(map(lambda x: "darknao/dotclear:%s" % (x,), tags)),
-      "context": "%s/%s" % (im['variant'], im['version']),
-      "dockerfile": im['file'],
-      })
+  if opts.set_output:
+    strategy['matrix']['include'].append(
+      {
+        "name": "%s-%s" % (im['version'], im['variant']),
+        "os": "ubuntu-latest",
+        "cache": "%s-cache" % (im['variant'],),
+        "tags": ",".join(map(lambda x: "darknao/dotclear:%s" % (x,), tags)),
+        "context": "%s/%s" % (im['variant'], im['version']),
+        "dockerfile": im['file'],
+        })
+  else:
+    strategy.append(
+      {
+        "name": "%s-%s" % (im['version'], im['variant']),
+        "tags": tags,
+        "dockerfile": im['file'],
+        })
 
-pprint(strategy)
-print("::set-output name=strategy::%s" % (json.dumps(strategy),))
+#pprint(strategy)
+if opts.set_output:
+  print("::set-output name=strategy::%s" % (json.dumps(strategy),))
+else:
+  print(json.dumps(strategy))
