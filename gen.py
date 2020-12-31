b6c6239821fc1f5337ba8b39109ef7231507d6ef
darknao <naolwen@gmail.com>
2020-12-31 01:51:53 +0100

    try em all

--- gen.py
+++ gen.py
@@ -1,23 +1,4 @@
 #!/usr/bin/env python
-
-"""
-matrix:
- include:
- - name: 1.2-apache
-   os: ubuntu-latest
-   tags:
-   - 1.2
-   - 1.2-apache
-   context: apache/1.2
- - name: 1.3-apache
-   os: ubuntu-latest
-   tags:
-   - 1.3
-   - 1.3-apache
-   - latest
-   context: apache/1.3
-"""
-
 import glob
 import json
 from distutils.version import StrictVersion
@@ -36,14 +17,13 @@ for df in dockerfiles:
 l.sort(key=lambda f: StrictVersion(f['version']))
 latest = l[-1]['version']
 print("latest is %s" % (latest,))
-pprint(l)
 strategy = {
   "max-parallel": 1,
   "fail-fast": False,
   "matrix": {
     "include": [],
     }}
-for im in l[:3]:
+for im in l:
   tags = []
   if im['variant'] == "apache":
     tags.append(im['version'])
